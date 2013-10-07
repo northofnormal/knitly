820949669ffa4d0daf838be830d2485caf98a849
Anne <northofnormal@gmail.com>
2013-10-07 09:46:49 -0400

    initial commit, here we go

---
+++ knitly/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knitly::Application.config.secret_key_base = '1ba3b5234892630969ad81a6ee927666f5efd250bbf712173489fe4909d249071e75ad23f823d60969980294d11294a511a7180b97c44f2e944c49483a83ce1c'
